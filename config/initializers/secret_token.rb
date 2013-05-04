eba7f99c420a6929fcae81aeba83fa1cb41c42f7
Starks <t2otrindie@gmail.com>
2013-05-04 11:18:24 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fukap::Application.config.secret_token = 'f4de088f18d910c9f9bcda94c83763d9fe7247804f15123380f60587a0c07bf3009b4037699f4b00a3fe1eccd850207964a7547c1251e068810a949376e6fdeb'
